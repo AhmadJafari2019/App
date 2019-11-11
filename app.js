365702cf609aabbd9f2ce218582d2b0dd906933d
Ahmad <ahmad.hazara@gmx.ch>
2019-11-12 00:20:53 +0100

    Write a function in the UI which has access to the input value and after we call it in the GLOBAL CONTROLLER

--- app.js
+++ app.js
@@ -1,34 +1,54 @@
 // Create the Module for the Data Structuring.
 // module keeps piece of code which is related together. Our code will be safe. 
 // Data Encapsulation allows us to hide the implementation details from a specific module  from outside the scope.
-
+// BUDGET CONTROLLER
 let budgetController = (function() {
-    let x = 23;
-    let add = function(a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b) {
-            return (add(b));
-        }
-    }
+    //  some code 
 })();
 
-//Creating another Module for the User Interface Data.
 
+
+// UI CONTROLLER.
 let UIController = (function() {
 
-})();
-
-//Create the Third module to connect the first and second module together.
-
-let controller = (function(budgetCtl, UICtl) {
-
-    let z = budgetCtl.publicTest(5);
     return {
-        anotherPublic: function() {
-            console.log(z);
+        getInput: function() {
+            return {
+                type: document.querySelector('.add__type').value, // Will be either inc or exp
+                description: document.querySelector('.add__description').value,
+                value: document.querySelector('.add__value').value,
+            };
+
+
         }
     }
+})();
+
+
+
+
+
+//GLOBAL APP CONTROLLER.
+let controller = (function(budgetCtl, UICtl) {
+    let ctrlAddItem = function() {
+        // 1. Get the input data.
+        let input = UICtl.getInput();
+        console.log(input);
+        // 2. Add the item to the budget controller.
+
+        // 3. Add the item to the UI 
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI.
+
+    }
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
+
 })(budgetController, UIController);
