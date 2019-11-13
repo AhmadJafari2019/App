de43885155b5d4f82c3920730b60e43a12c4aa3b
Ahmad <ahmad.hazara@gmx.ch>
2019-11-13 15:30:50 +0100

    Update the budget to display it in UI

--- app.js
+++ app.js
@@ -103,6 +103,10 @@ let UIController = (function() {
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
         expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__income--percentage',
 
     }
     return {
@@ -144,11 +148,13 @@ let UIController = (function() {
                 index.value = "";
             });
             fieldsArr[0].focus();
-
         },
-
-
-
+        displayBudget: function(obj) {
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+            document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
+        },
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -180,7 +186,7 @@ let controller = (function(budgetCtrl, UICtrl) {
         let budget = budgetCtrl.getBudget();
 
         // 3. Display the Budget on the UI.
-        console.log(budget);
+        UICtrl.displayBudget(budget);
     }
 
 
